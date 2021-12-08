fe9ad0af41931d71e34e7dcec87bd265928daaa3
Felipe Rozas Urrutia <ferozas@udec.cl>
2021-12-07 21:47:07 -0300

    jerarquia

--- script.js
+++ script.js
@@ -16,13 +16,31 @@ let aux = (numA, op, numb) => {
     } else if (op == "/") {
         return numA / numb;
     }
-    console.log('realiza funcion');
+    // console.log('realiza funcion');
 }
 
-
-
-
-
+let pap = (v) => {
+    let b = v;
+    // let c =[];
+    while (b.includes("X") || b.includes("/")) {
+        let c =[];
+        for (let i = 0; i < b.length; i++) {
+            if (b[i+1] != "X" && b[i+1] != "/") {
+                c.push(b[i]);
+                console.log(c);
+            } else {
+                c.push (aux (b[i], b[i+1], b[i+2]));
+                for (let j = i + 3; j < b.length; j++) {
+                    c.push (b[j]);
+                    console.log(c);
+                }
+                b = c;
+                break;
+            }
+        }
+    }
+    return b;
+}
 
 let Equal = () => {
     let lb = lastButton;
@@ -38,7 +56,7 @@ let Equal = () => {
             a.push (local1);
             local1 = "";
             a.push (lb[i]);
-            console.log(lb[i],"operacion");
+            // console.log(lb[i],"operacion");
         }
     }
     console.log (a); // create vector with strings
@@ -50,21 +68,17 @@ let Equal = () => {
     }
     console.log (a); // str to num
 
+    a = pap(a); // jer func :)
+
     let local2 = a[0];
     for (let i = 0; i<a.length; i++) {
         if (a[i]=="+" || a[i]=="-" || a[i]=="X" || a[i]=="/") {
             local2 = aux (local2, a[i],a[i+1]);
         }
     }
-    console.log (local2);
     lastButton=local2;
     screen.innerHTML = lastButton;
 }
-
-
-
-
-
 
 let Delete = () => {
     let a = "";
@@ -80,3 +94,7 @@ let Clear = () => {
     screen.innerHTML = lastButton;
 }
 
+
+
+// let gg = [12,"+",5,"X",6,"/",7,"-",8];
+// console.log (pap(gg))
